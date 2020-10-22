46941a52cc5688b2c052fcc81ecfd40b0224d53e
Elliot Åberg Fält <elliot.falt@gmail.com>
2020-10-22 14:48:22 +0200

    Added the ability to swap tiles/buttons and changed the winButton to correctly place all tiles instead to correctly test win condition

--- src/BrickGame.java
+++ src/BrickGame.java
@@ -15,104 +15,138 @@ import java.awt.event.ActionListener;
  */
 public class BrickGame extends JFrame {
     JPanel panel = new JPanel();
-    JButton button1 = new JButton("1");
-    JButton button2 = new JButton("2");
-    JButton button3 = new JButton("3");
-    JButton button4 = new JButton("4");
-    JButton button5 = new JButton("5");
-    JButton button6 = new JButton("6");
-    JButton button7 = new JButton("7");
-    JButton button8 = new JButton("8");
-    JButton button9 = new JButton("9");
-    JButton button10 = new JButton("10");
-    JButton button11 = new JButton("11");
-    JButton button12 = new JButton("12");
-    JButton button13 = new JButton("13");
-    JButton button14 = new JButton("14");
-    JButton button15 = new JButton("15");
-    JButton button16 = new JButton();
-    JButton newGame = new JButton("Nytt spel");
+    JButton newGameButton = new JButton("Nytt spel");
     List<JButton> buttonList;
     JButton selectedButton;
     //For testing purposes
-    JButton winButton = new JButton("Vin spelet (Debug)");
+    JButton winButton = new JButton("Sortera rätt (Debug)");
 
     public BrickGame() {
         //Add the panel and newGame button to the window.
         add("Center", panel);
-        add("North", newGame);
+        add("North", newGameButton);
         add("South", winButton);
 
         //Shuffle the the list of buttons and add them to the panel
-        buttonList = randomiseButtons();
-        panel.setLayout(new GridLayout(4,4));
-        for (JButton jButton : buttonList) {
-            panel.add(jButton);
-            jButton.addActionListener(new buttonListener());
-        }
-        newGame.addActionListener(new buttonListener());
+        panel.setLayout(new GridLayout(4, 4));
+        newGame();
+        newGameButton.addActionListener(new buttonListener());
         winButton.addActionListener(new buttonListener());
 
         //Finishing touches
         pack();
         setVisible(true);
-        setLocation(500,500);
+        setLocation(500, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     /**
      * Put all buttons into a list and shuffle the list
-     * @return the shuffled list
      */
-    private List<JButton> randomiseButtons() {
-        List<JButton> buttonList = new ArrayList<>();
-        buttonList.add(button1);
-        buttonList.add(button2);
-        buttonList.add(button3);
-        buttonList.add(button4);
-        buttonList.add(button5);
-        buttonList.add(button6);
-        buttonList.add(button7);
-        buttonList.add(button8);
-        buttonList.add(button9);
-        buttonList.add(button10);
-        buttonList.add(button11);
-        buttonList.add(button12);
-        buttonList.add(button13);
-        buttonList.add(button14);
-        buttonList.add(button15);
-        buttonList.add(button16);
+    private void randomiseButtons() {
+        buttonList = new ArrayList<>();
+        for (int i = 1; i < 16; i++) {
+            buttonList.add(new JButton(String.valueOf(i)));
+        }
+        buttonList.add(new JButton());
         Collections.shuffle(buttonList);
-        return buttonList;
     }
 
+    /**
+     * Show a message dialog that you have won the game and then exit
+     */
     private void winTheGame() {
         JOptionPane.showMessageDialog(null, "Du vann!");
         System.exit(0);
     }
 
-    class buttonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent ae) {
-            if (ae.getSource() != newGame && ae.getSource() != winButton) {
-                //TODO: Swap location of button
-            } else if (ae.getSource() == newGame){
-                buttonList = randomiseButtons();
-                panel.revalidate();
-                panel.repaint();
-                for (JButton jButton:buttonList) {
-                    panel.add(jButton);
-                    jButton.addActionListener(new buttonListener());
-                }
-                selectedButton = null;
+    /**
+     * Randomise all the buttons on the board again.
+     */
+    private void newGame() {
+        randomiseButtons();
+        panel.removeAll();
+        panel.revalidate();
+        for (JButton jButton : buttonList) {
+            panel.add(jButton);
+            jButton.addActionListener(new buttonListener());
+        }
+        selectedButton = null;
+    }
+
+    /**
+     * Searches the button list and returns the index of the empty block
+     *
+     * @return Index of the empty block
+     */
+    private int getEmpty() {
+        for (int i = 0; i < buttonList.size(); i++) {
+            if (buttonList.get(i).getText().equals("")) {
+                return i;
             }
-            else if (ae.getSource() == winButton) {
-                winTheGame();
-            }
+        }
+        return -1;
+    }
+
+    private void eligibleClick(JButton button) {
+        int index = buttonList.indexOf(button);
+        if (index % 4 != 0) {
+            checkEmpty(index - 1, button);
+        }
+
+        if (index - 4 >= 0) {
+            checkEmpty(index - 4, button);
+        }
+
+        if (index % 4 != 3) {
+            checkEmpty(index + 1, button);
+        }
+
+        if (index + 4 < buttonList.size()) {
+            checkEmpty(index + 4, button);
         }
     }
 
+    private void checkEmpty(int checkIndex, JButton button) {
+        JButton checkButton = buttonList.get(checkIndex);
+        if (checkButton.getText().equals("")) {
+            checkButton.setText(button.getText());
+            button.setText("");
+        }
+    }
 
+    /**
+     * Checks if the blocks are in the correct order (1-15 with empty being last) and
+     * runs the winTheGame method if they are.
+     */
+    private void checkWinCondition() {
+        for (int i = 0; i < buttonList.size() - 1; i++) {
+            JButton button = buttonList.get(i);
+            if (button.getText().equals("")) {
+                return;
+            } else if (Integer.parseInt(button.getText()) != (i + 1)) {
+                return;
+            }
+        }
+        winTheGame();
+    }
+
+    class buttonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if (ae.getSource() != newGameButton && ae.getSource() != winButton) {
+                eligibleClick((JButton) ae.getSource());
+                checkWinCondition();
+            } else if (ae.getSource() == newGameButton) {
+                newGame();
+            } else if (ae.getSource() == winButton) {
+                for (int i = 0; i < buttonList.size() - 1; i++) {
+                    buttonList.get(i).setText(String.valueOf(i + 1));
+                }
+                buttonList.get(15).setText("");
+            }
+        }
+    }
 
     public static void main(String[] args) {
         BrickGame b = new BrickGame();
